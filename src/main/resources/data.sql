22f35be039beb53c0f93608526be34d483faf67b
Airat-vagapov <airlake@yandex.ru>
2020-12-20 09:10:44 +0300

    correction data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,4 @@ INSERT INTO authors (id, name, picUrl) VALUES
 
 INSERT INTO albums (name, authors_id, year, count, length, liked, picUrl) VALUES
    ('good kid, mad city', 1, 2013, 15, 90, true, 'https://img.com/ljjjkd0213'),
-   ('Pimp to butterfly', 2, 2015, 12, 60, false, 'https://img.com/897213kla');
+   ('AlbumName', 2, 2015, 12, 60, false, 'https://img.com/897213kla');
